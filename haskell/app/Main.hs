423dce856c477be4274f15d50e2974ba077e7c75
Aner Lucero <4rgento@gmail.com>
2024-03-11 07:59:47 -0300

    Recover Day04

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -4,9 +4,11 @@ module Main where
 import qualified Day01 
 import qualified Day02
 import qualified Day03
+import qualified Day04
 
 main :: IO ()
 main = do
-  -- Day01.printResult "input/day-01/full.txt"
-  -- Day02.printResult "input/day-02/full.txt"
+  Day01.printResult "input/day-01/full.txt"
+  Day02.printResult "input/day-02/full.txt"
   Day03.printResult "input/day-03/full.txt"
+  Day04.printResult "input/day-04/full.txt"
--- haskell/lib/Day04.hs
+++ haskell/lib/Day04.hs
@@ -1,11 +1,33 @@
 module Day04 where
 
-import Crypto.Hash.MD5 (hash)
-import Data.ByteString (ByteString, pack, unpack)
+import Data.Digest.Pure.MD5 (md5)
 import Data.List (isPrefixOf, find)
+import Data.Attoparsec.ByteString.Char8 (letter_ascii, char)
+import Data.Attoparsec.ByteString (Parser, manyTill, eitherResult, parse)
+import qualified Data.ByteString.Char8 as B
+import Control.Monad ((>=>))
 
-part1 :: String -> Int -> Maybe Int
-part1 secretKey limit = find predicate [0..limit]
+solver :: Int -> Int -> String -> Maybe Int
+solver limit n key = find predicate [0..limit]
 	where
-	predicate :: Int -> Bool
-	predicate i = "00000" `isPrefixOf` (take 5 $ unpack _)
+	predicate = (limitStr `isPrefixOf`) . show . md5 .
+		B.fromStrict . B.pack . (key ++) .  show
+	limitStr = replicate n '0'
+
+part1 :: String -> Maybe Int
+part1 = solver (maxBound :: Int) 5
+
+part2 :: String -> Maybe Int
+part2 = solver (maxBound :: Int) 6
+
+inputParser :: Parser String
+inputParser = manyTill letter_ascii (char '\n')
+
+printResult :: FilePath -> IO ()
+printResult = B.readFile >=> either
+	(print . ("Error: " ++) . show)
+	(\input -> do
+		print $ part1 input
+		print $ part2 input
+	) .
+	eitherResult . parse inputParser
